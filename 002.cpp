1529befb49b3e50adb19e8b497ae362da2da7961
hellogqa <903078494@qq.com>
2019-07-25 16:52:28 +0800

    读取带透明通道的图像

--- 002.cpp
+++ 002.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
     Mat srcImage = imread("C:/OpenCV-4.1.0/opencv/sources/samples/data/opencv-logo.png", IMREAD_UNCHANGED);
+    // 读取带透明通道的图像时，参数选择IMREAD_UNCHANGED
 
     if (srcImage.empty())
     {
